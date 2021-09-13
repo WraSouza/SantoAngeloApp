62dbce9bc5f2b44aaa4515b154e09770a5e2ba08
Wladimir Ribeiro de Souza <wladimirsouza@outlook.com>
2021-09-13 08:24:41 -0300

    Primeiro commit.

---
+++ SantoAngelo/SantoAngelo/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SantoAngelo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage( new View.MainView() );
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SantoAngelo/SantoAngelo/View/MainView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SantoAngelo.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            var mapa = new Map(MapSpan.FromCenterAndRadius(new Position(-28.3049427, -54.2650362), Distance.FromKilometers(250)));
+            mapa.MapType = MapType.Street;
+
+            var SantoAngelo = new Pin()
+            {
+                Position = new Position(-28.3189302, -54.2484038),
+                Label = "Santo Ângelo"
+            };
+
+            mapa.Pins.Add(SantoAngelo);
+
+            MapContainer.Children.Add(mapa);
+        }
+    }
+}
